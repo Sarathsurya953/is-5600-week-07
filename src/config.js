abe37fe3ddc0618ff88440ffd2c443acef00cece
Sarath Surya Sunkara <143963343+Sarathsurya953@users.noreply.github.com>
2025-05-07 04:40:36 +0000

    Week 7 lab

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // TODO: Replace this with your own Server URL for your codespace
-export const BASE_URL = "https://glowing-space-memory-q7p4jvr9g4p424rj7-3080.app.github.dev";
+export const BASE_URL = "https://literate-space-goggles-v6v5wg9694j4cw6vj-3080.app.github.dev";
